6369955e41378a908e2c68d62be2e562a6ea74e5
luiz <viniciusadr244@gmai.com>
2024-09-13 21:43:38 -0300

    aula

--- atv1_csv.py
+++ atv1_csv.py
@@ -1,6 +1,8 @@
 import statistics as st
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import scipy.stats
 import seaborn as sns
 import pandas as pd
 
@@ -26,11 +28,34 @@ def show_column_info(database: pd.DataFrame, colum_name: str):
     print(f"Desvio padrão {colum_name}: {std :.2f}")
     print(f"Amplitude interquartil {colum_name}: {percentile[2] - percentile[0]}")
     print(f"{colum_name} outliers ({len(outliers)}): {outliers}")
-    # print(min(column_values), max(column_values))
+    print(min(column_values), max(column_values))
     # plt.hist(column_values, 5, (5, 80))
-    sns.boxplot([column_values, no_outliers], orient="h", fill=False)
-    plt.yticks([0,1], ['original', 'no outliers'])
+    # sns.boxplot([column_values, no_outliers], orient="h")
+    # plt.yticks([0,1], ['original', 'no outliers'])
+    
+    # Time series
+    # stock_copy = stock.copy()
+    # stock_copy['Date'] = pd.to_datetime(stock_copy['Date'])
+    # sns.lineplot(data=stock_copy, x='Date', y='High')
+    # sns.lineplot(data=stock_copy, x='Date', y='Low')
+
+
+    # Dispersão
+    # x = iris['SepalLengthCm']
+    # y = iris['SepalWidthCm']
+    
+    # print(scipy.stats.spearmanr(x, y)) # cria um ranking assimétrico
+    # # scipy.stats.pearsonr(x, y) # usa a média métrico
+    # # sns.boxplot(x)
+    # sns.scatterplot(x=x, y=y)
+    # column = iris['Species']
+    # print(iris.loc[column == 'Iris-setosa'])
+    # plt.show()
+
+    # heatmap
+    sns.heatmap(iris, annot=True)
     plt.show()
 
 
-show_column_info(iris, "SepalLengthCm")
+
+show_column_info(stock, "Close")
