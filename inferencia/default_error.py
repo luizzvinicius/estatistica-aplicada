6a7677ffc0dfc114832c6b45b3a16747e829e4ee
luiz <viniciusadr244@gmai.com>
2024-09-27 21:24:57 -0300

    feat: bootstrap

--- inferencia/default_error.py
+++ inferencia/default_error.py
@@ -20,11 +20,23 @@ intervalo = [media - 3 * default_error(array1), media + 3 * default_error(array1
 
 print("intervalo para média populacional:", intervalo)
 
-bootstrap_means = []
 
-for _ in range(400):
-    boot_sample = np.random.choice(array1, size=len(array1))
-    bootstrap_means.append(boot_sample.mean())
+def bootstrap(array, execucoes):
+    bootstrap_means = []
+    for _ in range(execucoes):
+        boot_sample = np.random.choice(array, size=len(array))
+        bootstrap_means.append(boot_sample.mean())
+    return bootstrap_means
 
-print("desvio das médias:", np.std(bootstrap_means, ddof=1))
-print(np.mean(bootstrap_means))
+print("desvio das médias:", np.std(bootstrap(array1, 400), ddof=1))
+print(np.mean(bootstrap(array1, 400)))
+
+# tempo para processar pedidos de emprestimo em h
+array2 = [24.1514, 27.4145, 20.4, 22.5151, 28.5152, 28.5611, 21.2489, 20.9983, 24.984, 22.6245]
+media2 = np.mean(array2)
+erro_padrao2 = default_error(array2)
+print(erro_padrao2)
+
+print("intervalo para média", [media2 - 3 * erro_padrao2, media2 + 3 * erro_padrao2])
+
+print(np.mean(bootstrap(array2, 400)))
