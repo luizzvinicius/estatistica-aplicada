f57bef8037b6c382eeee4e177620d2dc7c8314db
luiz <viniciusadr244@gmail.com>
2024-10-25 20:59:49 -0300

    feat: ssr

--- regression.py
+++ regression.py
@@ -10,3 +10,19 @@ print(model.coef_)
 print(model.intercept_)
 
 print(model.predict(x)) # preditores do y
+
+"""
+r²: valor entre 0 e 1 em porcentagem, quanto maior, menor o ssr, aqui que sabe se o erro está aceitável
+ssr: sempre na mão
+"""
+
+rsq = model.score(x,y)
+print(rsq)
+
+# print(model.predict(y) - y)
+
+def ssr():
+    result = sum(np.square(model.predict(x) - y))
+    print(result)
+
+ssr()
